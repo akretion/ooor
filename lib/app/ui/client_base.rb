f115e57c8b51ecf1c1ec0d1836cb57671845def4
Raphaël Valyi <rvalyi@akretion.com>
2010-04-28 04:16:01 -0300

    added AGPL v3 licence headers in source code

--- lib/app/ui/client_base.rb
+++ lib/app/ui/client_base.rb
@@ -1,3 +1,20 @@
+#    OOOR: Open Object On Rails
+#    Copyright (C) 2009-2010 Akretion LTDA (<http://www.akretion.com>).
+#    Author: Raphaël Valyi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'app/ui/form_model'
 require 'app/ui/menu'
 require 'app/ui/action_window'
