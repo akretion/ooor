e1a1f83868d1ebd1204503b1c991caaff21ceb83
Raphaël Valyi <rvalyi@akretion.com>
2011-01-09 19:06:47 -0200

    extracted XML/RPC transport client lib + made it compatible for Ruby 1.9 (force UTF-8 encoding) + nice exception raising

---
+++ lib/app/models/ooor_client.rb
@@ -0,0 +1,20 @@
+require 'xmlrpc/client'
+
+class OOORClient < XMLRPC::Client
+  def call2(method, *args)
+    request = create().methodCall(method, *args)
+	  data = (["<?xml version='1.0' encoding='UTF-8'?>\n"] + do_rpc(request, false).lines.to_a[1..-1]).join  #encoding is not defined by OpenERP and can lead to bug with Ruby 1.9
+    parser().parseMethodResponse(data)
+  rescue RuntimeError => e
+    begin
+      openerp_error_hash = eval("#{ e }".gsub("wrong fault-structure: ", ""))
+    rescue SyntaxError
+      raise e
+    end
+    if openerp_error_hash.is_a? Hash
+      raise RuntimeError.new "\n\n*********** OpenERP Server ERROR ***********\n#{openerp_error_hash["faultCode"]}\n#{openerp_error_hash["faultString"]}********************************************\n."
+    else
+      raise e
+    end
+  end
+end
