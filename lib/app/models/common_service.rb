e41bd2bd1b1813399faae6322b42c4219e66920b
Raphaël Valyi <rvalyi@akretion.com>
2011-10-06 14:30:32 -0300

    minor change: better login helper; specially for multi-session Rails apps

--- lib/app/models/common_service.rb
+++ lib/app/models/common_service.rb
@@ -18,19 +18,22 @@
 #proxies all 'common' class of OpenERP server/bin/service/web_service.py properly
 module Ooor
   module CommonService
+
+    def login(database, user, password)
+      get_rpc_client(@base_url + "/common").call("login", database, user, password)
+    end 
+
     def global_login(user, password)
       @config[:username] = user
       @config[:password] = password
-      @config[:user_id] = get_rpc_client(@base_url + "/common").call("login", @config[:database], user, password)
-      rescue Exception => error
-        @logger.error """login to OpenERP server failed:
+      @config[:user_id] = login(@config[:database], user, password)
+      rescue RuntimeError => error
+         @logger.error """login to OpenERP server failed:
          #{error.inspect}
          Are your sure the server is started? Are your login parameters correct? Can this server ping the OpenERP server?
          login XML/RPC url was #{@config[:url].gsub(/\/$/,'') + "/common"}"""
         raise
     end
-
-    def login(user, password); global_login(user, password); end
 
     #we generate methods handles for use in auto-completion tools such as jirb_swing
     [:ir_get, :ir_set, :ir_del, :about, :logout, :timezone_get, :get_available_updates, :get_migration_scripts, :get_server_environment, :login_message, :check_connectivity].each do |meth|
