9debdeca57c02a4c708260c207355874f718e75c
Raphaël Valyi <rvalyi@gmail.com>
2013-05-05 23:41:02 -0300

    minore refactoring

--- lib/ooor.rb
+++ lib/ooor.rb
@@ -9,51 +9,49 @@ module Ooor
   extend ActiveSupport::Autoload
   autoload :Connection
 
-  module ConnectionBehavior
+  module OoorBehavior
     extend ActiveSupport::Concern
+
+    attr_accessor :default_ooor, :default_config
+
     module ClassMethods
       #meant to be overriden in multi-tenant mode
       def connection(*args)
         a = b 
         default_ooor
       end
+
+      def new(*args)
+        Connection.send :new, *args
+      end
+
+      #load the custom configuration
+      def load_config(config_file=nil, env=nil)
+        config_file ||= defined?(Rails.root) && "#{Rails.root}/config/ooor.yml" || 'ooor.yml'
+        @config = YAML.load_file(config_file)[env || 'development']
+      rescue SystemCallError
+        puts """failed to load OOOR yaml configuration file.
+           make sure your app has a #{config_file} file correctly set up
+           if not, just copy/paste the default ooor.yml file from the OOOR Gem
+           to #{Rails.root}/config/ooor.yml and customize it properly\n\n"""
+        {}
+      end
+
+      def xtend(model_name, &block)
+        @extensions ||= {}
+        @extensions[model_name] ||= []
+        @extensions[model_name] << block
+        @extensions
+      end
+
+      def extensions
+        @extensions ||= {}
+      end
+
     end
   end
 
-  include ConnectionBehavior
-
-  class << self
-
-    attr_accessor :default_ooor, :default_config
-
-    def new(*args)
-      Connection.send :new, *args
-    end
-
-    #load the custom configuration
-    def load_config(config_file=nil, env=nil)
-      config_file ||= defined?(Rails.root) && "#{Rails.root}/config/ooor.yml" || 'ooor.yml'
-      @config = YAML.load_file(config_file)[env || 'development']
-    rescue SystemCallError
-      puts """failed to load OOOR yaml configuration file.
-         make sure your app has a #{config_file} file correctly set up
-         if not, just copy/paste the default ooor.yml file from the OOOR Gem
-         to #{Rails.root}/config/ooor.yml and customize it properly\n\n"""
-      {}
-    end
-
-    def xtend(model_name, &block)
-      @extensions ||= {}
-      @extensions[model_name] ||= []
-      @extensions[model_name] << block
-      @extensions
-    end
-
-    def extensions
-      @extensions ||= {}
-    end
-
-  end
+  include OoorBehavior
 end
 
 require 'ooor/railtie' if defined?(Rails)
