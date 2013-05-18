82d9e7a52fc31b1e9b382f4899cd4abc80e2c58c
Raphaël Valyi <rvalyi@gmail.com>
2013-05-18 05:07:20 -0300

    fixed meta-data

--- lib/ooor/reflection.rb
+++ lib/ooor/reflection.rb
@@ -5,18 +5,20 @@ module Ooor
   module Reflection # :nodoc:
     extend ActiveSupport::Concern
 
-    def set_columns_hash(view_fields={}) #FIXME force to compute if context + cache/expire?
-      @columns_hash = {}
-      @fields.each do |k, field|
-        unless @associations_keys.index(k)
-          @columns_hash[k] = field.merge({type: to_rails_type(view_fields[k] && view_fields[k]['type'] || field['type'])})
+    module ClassMethods
+      def set_columns_hash(view_fields={}) #FIXME force to compute if context + cache/expire?
+        @columns_hash = {}
+        @fields.each do |k, field|
+          unless @associations_keys.index(k)
+            @columns_hash[k] = field.merge({type: to_rails_type(view_fields[k] && view_fields[k]['type'] || field['type'])})
+          end
         end
+        @columns_hash
       end
-      @columns_hash
-    end
 
-    def column_for_attribute(name)
-      columns_hash[name.to_s]
+      def column_for_attribute(name)
+        columns_hash[name.to_s]
+      end
     end
 
   end
