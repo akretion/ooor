778d5b37a470f4ff4b59a9ca2bbf0e79238b3aa5
Raphaël Valyi <rvalyi@gmail.com>
2013-05-18 04:33:51 -0300

    less code complexity

--- lib/ooor/base.rb
+++ lib/ooor/base.rb
@@ -393,12 +393,19 @@ module Ooor
     def relationnal_result(method_name, *arguments)
       self.class.reload_fields_definition(false, object_session)
       if self.class.many2one_associations.has_key?(method_name)
-        return false unless @associations[method_name]
-        load_association(self.class.many2one_associations[method_name]['relation'], @associations[method_name].is_a?(Integer) && @associations[method_name] || @associations[method_name][0], *arguments)
+        if @associations[method_name]
+          rel = self.class.many2one_associations[method_name]['relation']
+          id = @associations[method_name].is_a?(Integer) ? @associations[method_name] : @associations[method_name][0]
+          load_association(rel, id, *arguments)
+        else
+          false
+        end
       elsif self.class.one2many_associations.has_key?(method_name)
-        load_association(self.class.one2many_associations[method_name]['relation'], @associations[method_name], *arguments) || []
+        rel = self.class.one2many_associations[method_name]['relation']
+        load_association(rel, @associations[method_name], *arguments) || []
       elsif self.class.many2many_associations.has_key?(method_name)
-        load_association(self.class.many2many_associations[method_name]['relation'], @associations[method_name], *arguments) || []
+        rel = self.class.many2many_associations[method_name]['relation']
+        load_association(rel, @associations[method_name], *arguments) || []
       elsif self.class.polymorphic_m2o_associations.has_key?(method_name)
         values = @associations[method_name].split(',')
         load_association(values[0], values[1].to_i, *arguments)
