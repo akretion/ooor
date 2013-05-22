d7a4dc27bfb9c12935f263b59ff7a75e0fe25b04
Raphaël Valyi <rvalyi@gmail.com>
2013-05-22 03:05:20 -0300

    much better OpenERP Server error management via new dedicated exception class

--- lib/ooor/errors.rb
+++ lib/ooor/errors.rb
@@ -15,4 +15,25 @@ module Ooor
       msg
     end
   end
+
+  class OpenERPServerError < RuntimeError
+    def initialize(error, method, *args)
+      begin
+        #extracts the eventual error log from OpenERP response as OpenERP doesn't enforce carefully*
+        #the XML/RPC spec, see https://bugs.launchpad.net/openerp/+bug/257581
+        openerp_error_hash = eval("#{error}".gsub("wrong fault-structure: ", ""))
+      rescue SyntaxError
+      end
+      if openerp_error_hash.is_a? Hash
+        if args[0].is_a?(String) && (args[1].is_a?(Integer) || args[1].to_i != 0) && args[2].is_a?(String)
+          args[2] = "####"
+        end
+        line = "********************************************"
+        message = "#{line}\n***********     OOOR Request     ***********\nmethod: #{method} - args: #{args.inspect}\n#{line}\n\n"
+        message << "\n#{line}\n*********** OpenERP Server ERROR ***********\n#{line}\n#{openerp_error_hash["faultCode"]}\n#{openerp_error_hash["faultString"]}#{line}\n."
+      end
+      super(message)
+    end
+  end
+
 end
--- lib/ooor/xml_rpc_client.rb
+++ lib/ooor/xml_rpc_client.rb
@@ -12,18 +12,7 @@ module Ooor
       data = (["<?xml version='1.0' encoding='UTF-8'?>\n"] + do_rpc(request, false).lines.to_a[1..-1]).join  #encoding is not defined by OpenERP and can lead to bug with Ruby 1.9
       parser().parseMethodResponse(data)
     rescue RuntimeError => e
-      begin
-        #extracts the eventual error log from OpenERP response as OpenERP doesn't enforce carefully*
-        #the XML/RPC spec, see https://bugs.launchpad.net/openerp/+bug/257581
-        openerp_error_hash = eval("#{ e }".gsub("wrong fault-structure: ", ""))
-      rescue SyntaxError
-        raise e
-      end
-      if openerp_error_hash.is_a? Hash
-        raise RuntimeError.new "\n\n*********** OpenERP Server ERROR ***********\n#{openerp_error_hash["faultCode"]}\n#{openerp_error_hash["faultString"]}********************************************\n."
-      else
-        raise e
-      end
+      raise OpenERPServerError.new(e, method, *args)
     end
   end
 end
