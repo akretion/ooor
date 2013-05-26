e0647163dd66fcbddf8f32105341d705acfd9c79
Raphaël Valyi <rvalyi@gmail.com>
2013-05-26 15:45:46 -0300

    added missing file

---
+++ lib/ooor/services.rb
@@ -0,0 +1,55 @@
+#    OOOR: OpenObject On Ruby
+#    Copyright (C) 2009-2013 Akretion LTDA (<http://www.akretion.com>).
+#    Author: Raphaël Valyi
+#    Licensed under the MIT license, see MIT-LICENSE file
+
+module Ooor
+  class Service
+    def self.define_service(service, methods)
+      methods.each do |meth|
+        self.instance_eval do
+          define_method meth do |*args|
+            args[-1] = @connection.connection_session.merge(args[-1]) if args[-1].is_a? Hash
+            @connection.get_rpc_client("#{@connection.base_url}/#{service}").call(meth, *args)
+          end
+        end
+      end
+    end
+
+    def initialize(connection)
+      @connection = connection
+    end
+  end
+
+
+  class CommonService < Service
+    define_service(:common, %w[ir_get ir_set ir_del about login logout timezone_get get_available_updates get_migration_scripts get_server_environment login_message check_connectivity about get_stats list_http_services version authenticate get_available_updates set_loglevel get_os_time get_sqlcount])
+  end
+
+
+  class DbService < Service
+    define_service(:db, %w[get_progress drop dump restore rename db_exist list change_admin_password list_lang server_version migrate_databases create_database duplicate_database])
+
+    def create(password=@config[:db_password], db_name='ooor_test', demo=true, lang='en_US', user_password=@config[:password] || 'admin')
+      @logger.info "creating database #{db_name} this may take a while..."
+      process_id = get_rpc_client(base_url + "/db").call("create", password, db_name, demo, lang, user_password)
+      sleep(2)
+      while get_progress(password, process_id)[0] != 1
+        @logger.info "..."
+        sleep(0.5)
+      end
+      global_login('admin', user_password, db_name, false)
+    end
+  end
+
+
+  class ObjectService < Service
+    define_service(:object, %w[execute exec_workflow])
+  end
+
+
+  class ObjectService < Service
+    define_service(:report, %w[report report_get render_report])
+  end
+
+end
