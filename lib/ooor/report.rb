5e326a2deacdebf366917ab1fa467d583ce68904
Hugo Santos <hugo.santos@factorlibre.com>
2013-11-19 18:41:31 +0100

    Fix on report print

--- lib/ooor/report.rb
+++ lib/ooor/report.rb
@@ -9,12 +9,12 @@ module Ooor
       def report(report_name, ids, report_type='pdf', context={}) #TODO move to ReportService
         database, user_id, password, context = credentials_from_args(context)
         params = {model: @openerp_model, id: ids[0], report_type: report_type}
-        connection.report(database, user_uid, password, password, report_name, ids, params, context)
+        connection.report.report(database, user_id, password, report_name, ids, params)
       end
 
       def report_get(report_id, context={})
         database, user_id, password, context = credentials_from_args(context)
-        connection.report_get(database, user_uid, password, password, report_id)
+        connection.report.report_get(database, user_id, password, report_id)
       end
 
       def get_report_data(report_name, ids, report_type='pdf', context={})
@@ -41,6 +41,37 @@ module Ooor
           logger.debug "OOOR RPC: 'report not found'"
         end
         return nil
+      end
+
+      def credentials_from_context(*args)
+        if args[-1][:context_index]
+          i = args[-1][:context_index]
+          args.delete_at -1
+        else
+          i = -1
+        end
+        c = HashWithIndifferentAccess.new(args[i])
+        user_id = c.delete(:ooor_user_id) || @connection.config[:user_id]
+        password = c.delete(:ooor_password) || @connection.config[:password]
+        database = c.delete(:ooor_database) || @connection.config[:database]
+        args[i] = @connection.connection_session.merge(c)
+        return database, user_id, password, args
+      end
+
+      def credentials_from_args(*args)
+        if args[-1].is_a? Hash #context
+          database, user_id, password, args = credentials_from_context(*args)
+        else
+          user_id = @connection.config[:user_id]
+          password = @connection.config[:password]
+          database = @connection.config[:database]
+        end
+        if user_id.is_a?(String) && user_id.to_i == 0
+          user_id = Ooor.cache.fetch("login-id-#{user_id}") do
+            @connection.common.login(database, user_id, password)
+          end
+        end
+        return database, user_id.to_i, password, args
       end
 
     end
