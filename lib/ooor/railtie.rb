2b5d7da8d0994198eebb9afc0fe3b3828e7a150d
Raphaël Valyi <rvalyi@gmail.com>
2013-12-31 18:02:39 -0200

    improvement around locale management in Rack middleware; but warning I18nLocaleSwitcher shouln't be included in a Rails app because it makes a strange locale conflict; fixed totally soon hopefully

--- lib/ooor/railtie.rb
+++ lib/ooor/railtie.rb
@@ -1,13 +1,22 @@
 require "rails/railtie"
+require "ooor/rack"
 
 module Ooor
   class Railtie < Rails::Railtie
     initializer "ooor.middleware" do |app|
       Ooor.default_config = load_config(false, Rails.env)
       connection = Ooor::Base.connection_handler.retrieve_connection(Ooor.default_config)
-      if Ooor.default_config['bootstrap']
+      if Ooor.default_config[:bootstrap]
         connection.global_login(config)
       end
+      unless Ooor.default_config[:disable_locale_switcher]
+        if defined?(Rack::I18nLocaleSwitcher)
+          app.middleware.use '::Rack::I18nLocaleSwitcher'
+        else
+          puts "Could not load Rack::I18nLocaleSwitcher, if your application is internationalized, make sure to include rack-i18n_locale_switcher in your Gemfile"
+        end
+      end
+      app.middleware.use '::Ooor::Rack'
     end
 
     def load_config(config_file=nil, env=nil)
