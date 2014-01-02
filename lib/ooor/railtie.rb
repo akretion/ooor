d12668a45bf68b2c4f69b7dee41aca89545b928a
Raphaël Valyi <rvalyi@gmail.com>
2014-01-02 02:47:04 -0200

    removed comment

--- lib/ooor/railtie.rb
+++ lib/ooor/railtie.rb
@@ -16,7 +16,6 @@ module Ooor
           puts "Could not load Rack::I18nLocaleSwitcher, if your application is internationalized, make sure to include rack-i18n_locale_switcher in your Gemfile"
         end
       end
-#      app.middleware.use '::Ooor::Rack' #TODO put app.insert_after ActionDispatch::ParamsParser, '::Ooor::Rack' ?
       app.middleware.insert_after ActionDispatch::ParamsParser, '::Ooor::Rack'
     end
 
