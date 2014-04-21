3a8b359be3dac64d190bebeba27c551d24055974
Raphaël Valyi <rvalyi@gmail.com>
2014-04-21 00:38:09 -0300

    more modular type casting to OpenERP associations

--- lib/ooor/type_casting.rb
+++ lib/ooor/type_casting.rb
@@ -8,6 +8,7 @@ module Ooor
     extend ActiveSupport::Concern
     
     OPERATORS = ["=", "!=", "<=", "<", ">", ">=", "=?", "=like", "=ilike", "like", "not like", "ilike", "not ilike", "in", "not in", "child_of"]
+    BLACKLIST = %w[id write_date create_date write_ui create_ui]
   
     module ClassMethods
       
@@ -120,65 +121,73 @@ module Ooor
       
     end
     
-      def sanitize_attribute(skey, value)
-        type = self.class.fields[skey]['type']
-        if type == 'boolean' && value == 1 || value == "1"
-          true
-        elsif type == 'boolean'&& value == 0 || value == "0"
-          false
-        elsif value == false and type != 'boolean'
-          nil
-        else
-         value
-        end
+    def sanitize_attribute(skey, value)
+      type = self.class.fields[skey]['type']
+      if type == 'boolean' && value == 1 || value == "1"
+        true
+      elsif type == 'boolean'&& value == 0 || value == "0"
+        false
+      elsif value == false and type != 'boolean'
+        nil
+      else
+        value
       end
+    end
 
-      def sanitize_association(skey, value)
-        if value.is_a?(Ooor::Base) || value.is_a?(Array) && value.all? {|i| i.is_a?(Ooor::Base)}
-          value
-        elsif value.is_a?(Array) && !self.class.many2one_associations.keys.index(skey)
-          value.reject {|i| i == ''}.map {|i| i.is_a?(String) ? i.to_i : i}
-        elsif value.is_a?(String)
-          if self.class.polymorphic_m2o_associations.has_key?(skey)
-            value
-          elsif self.class.many2one_associations.has_key?(skey)
-            if value.blank? || value == "0"
-              false
-            else
-              value.to_i
-            end
-          else
-            value.split(",").map {|i| i.to_i}
-          end
-        else
-          value
-        end
+    def sanitize_association(skey, value)
+      if value.is_a?(Ooor::Base) || value.is_a?(Array) && value.all? {|i| i.is_a?(Ooor::Base)}
+        value
+      elsif value.is_a?(Array) && !self.class.many2one_associations.keys.index(skey)
+        value.reject {|i| i == ''}.map {|i| i.is_a?(String) ? i.to_i : i}
+      elsif value.is_a?(String)
+        sanitize_associatio_as_string(skey, value)
+      else
+        value
       end
+    end
+
+    def sanitize_associatio_as_string(skey, value)
+      if self.class.polymorphic_m2o_associations.has_key?(skey)
+        value
+      elsif self.class.many2one_associations.has_key?(skey)
+        sanitize_m2o_association(value)
+      else
+        value.split(",").map {|i| i.to_i}
+      end
+    end
+
+    def sanitize_m2o_association(value)
+      if value.blank? || value == "0"
+        false
+      else
+        value.to_i
+      end
+    end
 
     def to_openerp_hash
-      associations = {}
+      attributes, associations = get_changed_values
+      associations = cast_associations_to_openerp(associations)
+      attributes.merge(associations)
+    end
+    
+    def get_changed_values
       attributes = {}
+      associations = {}
 
       changed.each do |k|
         if self.class.associations_keys.index(k)
           associations[k] = @associations[k]#changes[k][1]
         elsif self.class.fields.has_key?(k)
           attributes[k]= @attributes[k]
-        else
+        elsif !BLACKLIST.index(k)
           attributes[k] = changes[k][1]
         end
       end
-      associations = cast_associations_to_openerp(associations)
-      blacklist = %w[id write_date create_date write_ui create_ui]
-      r = {}
-      attributes.reject {|k, v| blacklist.index(k)}.merge(associations).each do |k, v|
-        r[k] = v
-      end
-      r
+      return attributes, associations
     end
-    
+
     def cast_associations_to_openerp(associations=@associations)
-      associations.each do |k, v| #see OpenERP awkward associations API
+      associations.each do |k, v|
         associations[k] = self.cast_association(k, v)
       end
     end
@@ -186,27 +195,35 @@ module Ooor
     # talk OpenERP cryptic associations API
     def cast_association(k, v)
       if self.class.one2many_associations[k]
-        v.collect do |value|
-          if value.is_a?(Base)
-            [0, 0, value.to_openerp_hash]
-          else
-            if value.is_a?(Hash)
-              [0, 0, value]
-            else
-              [1, value, {}]
-            end
-          end
-        end
+        cast_o2m_assocation(v)
       elsif self.class.many2many_associations[k]
         [[6, false, (v || []).map {|i| i.is_a?(Base) ? i.id : i}]]
       elsif self.class.many2one_associations[k]
-        if v.is_a?(Array)
-          v[0]
-        elsif v.is_a?(Base)
-          v.id
+        cast_m2o_association(v)
+      end
+    end
+
+    def cast_o2m_assocation(v)
+      v.collect do |value|
+        if value.is_a?(Base)
+          [0, 0, value.to_openerp_hash]
         else
-          v
+          if value.is_a?(Hash)
+            [0, 0, value]
+          else
+            [1, value, {}]
+          end
         end
+      end
+    end
+
+    def cast_m2o_association(v)
+      if v.is_a?(Array)
+        v[0]
+      elsif v.is_a?(Base)
+        v.id
+      else
+        v
       end
     end
  
