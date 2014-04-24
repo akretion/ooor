5b9b54c4f74516e0e31e3977342f1ed13f119767
Raphaël Valyi <rvalyi@gmail.com>
2014-04-24 11:29:48 -0300

    some code documentation love

--- lib/ooor/mini_active_resource.rb
+++ lib/ooor/mini_active_resource.rb
@@ -3,6 +3,13 @@ require 'active_support/core_ext/class/attribute_accessors'
 require 'active_model'
 
 module Ooor
+  # Ooor::MiniActiveResource is a shrinked version of ActiveResource::Base with the bare minimum we need for Ooor.
+  # as a reminder ActiveResource::Base is the main class for mapping RESTful resources as models in a Rails application.
+  # Ooor is a bit like ActiveResource but eventually it can use more OpenERP metadata and a richer API
+  # to become closer to ActiveRecord or Mongoid than Activeresource. Also OpenERP isn't really good at REST
+  # so the part of ActiveResource dedicated to REST is of little help here.
+  # An other fundamental difference is Ooor is multi OpenERP instances and multi-sessions.
+  # for each session, proxies to OpenERP may be different.
   class MiniActiveResource
 
     class << self
@@ -21,6 +28,7 @@ module Ooor
       super({ :root => self.class.element_name }.merge(options))
     end
 
+    # Returns +true+ if this object hasn't yet been saved, otherwise, returns +false+.
     def new?
       !@persisted
     end
@@ -39,6 +47,10 @@ module Ooor
       attributes["id"] = id
     end
 
+    # Reloads the record from the database.
+    #
+    # This method finds record by its primary key (which could be assigned manually) and
+    # modifies the receiver in-place
     def reload
       self.class.find(id)
     end
