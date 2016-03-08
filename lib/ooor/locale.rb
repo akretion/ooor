7c1a0cae71f2c500bd3b08b25456e029779e7628
Raphaël Valyi <rvalyi@akretion.com>
2016-03-08 05:41:24 -0300

    simplifed to_erp_locale code

--- lib/ooor/locale.rb
+++ lib/ooor/locale.rb
@@ -1,10 +1,8 @@
 module Ooor
   module Locale
-    # OpenERP requirs a locale+zone mapping while Rails uses locale only, so mapping is likely to be required
+    # Odoo requires a locale+zone mapping while Rails uses locale only, so mapping is likely to be required
     def self.to_erp_locale(locale)
-      if Ooor.default_config[:locale_mapping]
-        mapping = Ooor.default_config[:locale_mapping]
-      else
+      unless mapping = Ooor.default_config[:locale_mapping]
         mapping = {'fr' => 'fr_FR', 'en' => 'en_US'}
       end
       (mapping[locale.to_s] || locale.to_s).gsub('-', '_')
